413f86173e9490389ee9b8eb3e3e4dd5974c7636
leon <leon@im30.lan>
2024-05-14 19:10:06 +0800

    [feature]ecs

---
+++ ECS/Assets/Sample/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿// **********************************************************
+// *		                .-"""-.							*
+// *		               / .===. \			            *
+// *		               \/ 6 6 \/			            *
+// *		     ______ooo__\__=__/_____________			*
+// *		    / @author     Leon			   /			*
+// *		   / @Modified   2024-05-13       /			    *
+// *		  /_____________________ooo______/			    *
+// *		  			    |_ | _|			                *
+// *		  			    /-'Y'-\			                *
+// *		  			   (__/ \__)			            *
+// **********************************************************
+
+using Unity.Entities;
+using Unity.Mathematics;
+
+
+public struct Spawner : IComponentData
+{
+	public Entity Prefab;
+	public float3 SpawnPosition;
+	public float NextSpawnTime;
+	public float SpawnRate;
+}
